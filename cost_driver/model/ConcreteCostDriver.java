2e8aa5c9931a41b89e57428b8c6ddf4dd4f39666
Wen <ge42yuc@mytum.de>
2024-08-21 16:10:42 +0200

    add getting and attributes

--- cost_driver/model/ConcreteCostDriver.java
+++ cost_driver/model/ConcreteCostDriver.java
@@ -2,14 +2,35 @@ package cost_driver.model;
 
 import de.hpi.bpt.scylla.exception.ScyllaValidationException;
 
+import javax.xml.datatype.Duration;
+
 public class ConcreteCostDriver extends costDriver{
     protected costDriver parent;
+    protected Double probability;
+    protected Double LCAScore;
+    protected Duration duration;
 
-    public ConcreteCostDriver(String id, costDriver parent) throws ScyllaValidationException {
+    public ConcreteCostDriver(String id, costDriver parent, Double probability, Double LCAScore, Duration duration) throws ScyllaValidationException {
         super(id);
         if (parent instanceof AbstractCostDriver) {
             this.parent = parent;
         } else { throw new ScyllaValidationException("Parent cost driver is not abstract");}
+
+        this.duration = duration;
+        this.LCAScore = LCAScore;
+        this.probability = probability;
+    }
+
+    public Double getProbability() {
+        return probability;
+    }
+
+    public Double getLCAScore() {
+        return LCAScore;
+    }
+
+    public Duration getDuration() {
+        return duration;
     }
 
     public costDriver getParent() {
